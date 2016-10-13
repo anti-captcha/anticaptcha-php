dc67596cc1785832b47b62c85caf0f2c46854140
Anti Admin <admin@anti-captcha.com>
2016-10-13 06:13:36 -0400

    Added ImageToText example, renamed example.php to exampleNoCaptcha.php

---
+++ exampleImageToText.php
@@ -0,0 +1,28 @@
+<?php
+
+include("anticaptcha.php");
+include("imagetotext.php");
+
+$api = new ImageToText();
+$api->setVerboseMode(true);
+        
+//your anti-captcha.com account key
+$api->setKey("12345678901234567890123456789012");
+
+//setting file
+$api->setFile("capcha.jpg");
+
+if (!$api->createTask()) {
+    $api->debout("API v2 send failed - ".$api->getErrorMessage(), "red");
+    return false;
+}
+
+$taskId = $api->getTaskId();
+
+
+if (!$api->waitForResult()) {
+    $api->debout("could not solve captcha", "red");
+    $api->debout($api->getErrorMessage());
+} else {
+    echo "\nhash result: ".$api->getTaskSolution()."\n\n";
+}
