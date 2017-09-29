5521bfbb236d28c064b101d5df3f5ecf8b65b5b4
traffic <traffic@anticaptcha.vps>
2017-09-29 17:09:30 +0000

    Added example for FunCaptcha

---
+++ funcaptcha.php
@@ -0,0 +1,73 @@
+<?php
+
+class FunCaptcha extends Anticaptcha implements AntiCaptchaTaskProtocol {
+
+    private $websiteUrl;
+    private $websitePublicKey;
+    private $proxyType = "http";
+    private $proxyAddress;
+    private $proxyPort;
+    private $proxyLogin;
+    private $proxyPassword;
+    private $userAgent = "";
+    private $cookies = "";
+    
+    public function getPostData() {
+        return array(
+            "type"              =>  "FunCaptchaTask",
+            "websiteURL"        =>  $this->websiteUrl,
+            "websitePublicKey"  =>  $this->websitePublicKey,
+            "proxyType"         =>  $this->proxyType,
+            "proxyAddress"      =>  $this->proxyAddress,
+            "proxyPort"         =>  $this->proxyPort,
+            "proxyLogin"        =>  $this->proxyLogin,
+            "proxyPassword"     =>  $this->proxyPassword,
+            "userAgent"         =>  $this->userAgent
+        );
+    }
+    
+    public function setTaskInfo($taskInfo) {
+        $this->taskInfo = $taskInfo;
+    }
+    
+    public function getTaskSolution() {
+        return $this->taskInfo->solution->token;
+    }
+    
+    public function setWebsiteURL($value) {
+        $this->websiteUrl = $value;
+    }
+    
+    public function setWebsitePublicKey($value) {
+        $this->websitePublicKey = $value;
+    }
+    
+    public function setProxyType($value) {
+        $this->proxyType = $value;
+    }
+    
+    public function setProxyAddress($value) {
+        $this->proxyAddress = $value;
+    }
+    
+    public function setProxyPort($value) {
+        $this->proxyPort = $value;
+    }
+    
+    public function setProxyLogin($value) {
+        $this->proxyLogin = $value;
+    }
+    
+    public function setProxyPassword($value) {
+        $this->proxyPassword = $value;
+    }
+    
+    public function setUserAgent($value) {
+        $this->userAgent = $value;
+    }
+    
+    public function setCookies($value) {
+        $this->cookies = $value;
+    }
+    
+}
