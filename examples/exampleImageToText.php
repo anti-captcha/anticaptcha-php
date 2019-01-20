bd7a64f0b9ccfc7dd75b82dc59431a2e61fd1a59
Anti Admin <robot@antigate.com>
2019-01-20 11:24:43 +1030

    moved examples to subdirectory

---
+++ examples/exampleImageToText.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../anticaptcha.php");
+include("../imagetotext.php");
+
+$api = new ImageToText();
+$api->setVerboseMode(true);
+        
+//your anti-captcha.com account key
+$api->setKey(readline("You API key: "));
+
+//setting file
+$api->setFile("capcha.jpg");
+
+if (!$api->createTask()) {
+    $api->debout("API v2 send failed - ".$api->getErrorMessage(), "red");
+    return false;
+}
+
+$taskId = $api->getTaskId();
+
+
+if (!$api->waitForResult()) {
+    $api->debout("could not solve captcha", "red");
+    $api->debout($api->getErrorMessage());
+} else {
+    $captchaText    =   $api->getTaskSolution();
+    echo "\nresult: $captchaText\n\n";
+}
