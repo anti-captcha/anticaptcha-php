19c575d35bd89521021483b51370a429bd521ad5
Anti Admin <robot@antigate.com>
2023-01-05 17:20:26 +0700

    Added AntiBotCookieTask support

---
+++ antibotcookie.php
@@ -0,0 +1,60 @@
+<?php
+
+class AntiBotCookie extends Anticaptcha implements AntiCaptchaTaskProtocol {
+
+    private $websiteUrl;
+    private $providerName;
+    private $proxyAddress;
+    private $proxyPort;
+    private $proxyLogin;
+    private $proxyPassword;
+    
+    public function getPostData() {
+        return array(
+            "type"                      =>  "AntiBotCookieTask",
+            "providerName"              =>  $this->providerName,
+            "websiteURL"                =>  $this->websiteUrl,
+            "proxyAddress"              =>  $this->proxyAddress,
+            "proxyPort"                 =>  $this->proxyPort,
+            "proxyLogin"                =>  $this->proxyLogin,
+            "proxyPassword"             =>  $this->proxyPassword
+        );
+    }
+    
+    public function setTaskInfo($taskInfo) {
+        $this->taskInfo = $taskInfo;
+    }
+    
+    public function getTaskSolution() {
+        return $this->taskInfo->solution;
+    }
+    
+    public function setWebsiteURL($value) {
+        $this->websiteUrl = $value;
+    }
+    
+    public function setProviderName($value) {
+        if (!is_string($value)) {
+            echo "Provider name must be a string!";
+            exit;
+        }
+        $this->providerName = $value;
+    }
+    
+    public function setProxyAddress($value) {
+        $this->proxyAddress = $value;
+    }
+    
+    public function setProxyPort($value) {
+        $this->proxyPort = $value;
+    }
+    
+    public function setProxyLogin($value) {
+        $this->proxyLogin = $value;
+    }
+    
+    public function setProxyPassword($value) {
+        $this->proxyPassword = $value;
+    }
+    
+}
